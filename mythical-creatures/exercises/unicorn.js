d38b6012fc68d9cf8673a4f93aea70797d60450b
Jeff Kersting <jeff.w.kersting@gmail.com>
2020-10-12 16:58:40 -0600

    Add completed unicorn class

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -2,6 +2,16 @@ class Unicorn {
   constructor(name, color) {
     this.name = name;
     this.color = color;
+
+    if(color == undefined){
+        this.color = 'white'
+    }
+  }
+  isWhite(){
+    return this.color === 'white'
+  }
+  says(wordsSaid) {
+    return `**;* ${wordsSaid} *;**`
   }
 }
 
