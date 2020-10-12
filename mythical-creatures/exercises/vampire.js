04634020bc1cad30a19d0ebcadbff26e64663bc8
Jeff Kersting <jeff.w.kersting@gmail.com>
2020-10-12 17:15:59 -0600

    Add completed vampire class

---
+++ mythical-creatures/exercises/vampire.js
@@ -0,0 +1,16 @@
+class Vampire {
+  constructor(name, pet){
+    this.name = name;
+    this.pet = pet;
+    this.thirsty = true;
+
+    if(this.pet === undefined){
+      this.pet = "bat"
+    }
+  }
+  drink(){
+    this.thirsty = false
+  }
+}
+
+module.exports = Vampire;
